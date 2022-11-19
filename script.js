c9fe66ec1c1581e88750e24e03cd326575d9b4d0
itush <itusharbiswas@gmail.com>
2022-11-20 01:26:22 +0530

    draft-1

--- script.js
+++ script.js
@@ -9,65 +9,65 @@ fetch("./data.json")
 
     function dailyData() {
         /**daily Work-Data**/
-        document.querySelector(".w-current").innerText = data[0].timeframes.daily.current;
-        document.querySelector(".w-previous").innerText = data[0].timeframes.daily.previous;
+        document.querySelector(".w-current").innerText = data[0].timeframes.daily.current+`hrs`;
+        document.querySelector(".w-previous").innerText = data[0].timeframes.daily.previous+`hrs`;
         /**daily Play-Data**/
-        document.querySelector(".p-current").innerText = data[1].timeframes.daily.current;
-        document.querySelector(".p-previous").innerText = data[1].timeframes.daily.previous;
+        document.querySelector(".p-current").innerText = data[1].timeframes.daily.current+`hrs`;
+        document.querySelector(".p-previous").innerText = data[1].timeframes.daily.previous+`hrs`;
         /**daily Study-Data**/
-        document.querySelector(".s-current").innerText = data[2].timeframes.daily.current;
-        document.querySelector(".s-previous").innerText = data[2].timeframes.daily.previous;
+        document.querySelector(".s-current").innerText = data[2].timeframes.daily.current+`hrs`;
+        document.querySelector(".s-previous").innerText = data[2].timeframes.daily.previous+`hrs`;
         /**daily Exercise-Data**/
-        document.querySelector(".e-current").innerText = data[3].timeframes.daily.current;
-        document.querySelector(".e-previous").innerText = data[3].timeframes.daily.previous;
+        document.querySelector(".e-current").innerText = data[3].timeframes.daily.current+`hrs`;
+        document.querySelector(".e-previous").innerText = data[3].timeframes.daily.previous+`hrs`;
         /**daily Social-Data**/
-        document.querySelector(".so-current").innerText = data[4].timeframes.daily.current;
-        document.querySelector(".so-previous").innerText = data[4].timeframes.daily.previous;
+        document.querySelector(".so-current").innerText = data[4].timeframes.daily.current+`hrs`;
+        document.querySelector(".so-previous").innerText = data[4].timeframes.daily.previous+`hrs`;
         /**daily Self Care-Data**/
-        document.querySelector(".se-current").innerText = data[5].timeframes.daily.current;
-        document.querySelector(".se-previous").innerText = data[5].timeframes.daily.previous;
+        document.querySelector(".se-current").innerText = data[5].timeframes.daily.current+`hrs`;
+        document.querySelector(".se-previous").innerText = data[5].timeframes.daily.previous+`hrs`;
     }
 
     function weeklyData() {
         /**weekly Work-Data**/
-        document.querySelector(".w-current").innerText = data[0].timeframes.weekly.current;
-        document.querySelector(".w-previous").innerText = data[0].timeframes.weekly.previous;
+        document.querySelector(".w-current").innerText = data[0].timeframes.weekly.current+`hrs`;
+        document.querySelector(".w-previous").innerText = data[0].timeframes.weekly.previous+`hrs`;
         /**weekly Play-Data**/
-        document.querySelector(".p-current").innerText = data[1].timeframes.weekly.current;
-        document.querySelector(".p-previous").innerText = data[1].timeframes.weekly.previous;
+        document.querySelector(".p-current").innerText = data[1].timeframes.weekly.current+`hrs`;
+        document.querySelector(".p-previous").innerText = data[1].timeframes.weekly.previous+`hrs`;
         /**weekly Study-Data**/
-        document.querySelector(".s-current").innerText = data[2].timeframes.weekly.current;
-        document.querySelector(".s-previous").innerText = data[2].timeframes.weekly.previous;
+        document.querySelector(".s-current").innerText = data[2].timeframes.weekly.current+`hrs`;
+        document.querySelector(".s-previous").innerText = data[2].timeframes.weekly.previous+`hrs`;
         /**weekly Exercise-Data**/
-        document.querySelector(".e-current").innerText = data[3].timeframes.weekly.current;
-        document.querySelector(".e-previous").innerText = data[3].timeframes.weekly.previous;
+        document.querySelector(".e-current").innerText = data[3].timeframes.weekly.current+`hrs`;
+        document.querySelector(".e-previous").innerText = data[3].timeframes.weekly.previous+`hrs`;
         /**weekly Social-Data**/
-        document.querySelector(".so-current").innerText = data[4].timeframes.weekly.current;
-        document.querySelector(".so-previous").innerText = data[4].timeframes.weekly.previous;
+        document.querySelector(".so-current").innerText = data[4].timeframes.weekly.current+`hrs`;
+        document.querySelector(".so-previous").innerText = data[4].timeframes.weekly.previous+`hrs`;
         /**weekly Self Care-Data**/
-        document.querySelector(".se-current").innerText = data[5].timeframes.weekly.current;
-        document.querySelector(".se-previous").innerText = data[5].timeframes.weekly.previous;
+        document.querySelector(".se-current").innerText = data[5].timeframes.weekly.current+`hrs`;
+        document.querySelector(".se-previous").innerText = data[5].timeframes.weekly.previous+`hrs`;
     }
 
     function monthlyData() {
         /**monthly Work-Data**/
-        document.querySelector(".w-current").innerText = data[0].timeframes.monthly.current;
-        document.querySelector(".w-previous").innerText = data[0].timeframes.monthly.previous;
+        document.querySelector(".w-current").innerText = data[0].timeframes.monthly.current+`hrs`;
+        document.querySelector(".w-previous").innerText = data[0].timeframes.monthly.previous+`hrs`;
         /**monthly Play-Data**/
-        document.querySelector(".p-current").innerText = data[1].timeframes.monthly.current;
-        document.querySelector(".p-previous").innerText = data[1].timeframes.monthly.previous;
+        document.querySelector(".p-current").innerText = data[1].timeframes.monthly.current+`hrs`;
+        document.querySelector(".p-previous").innerText = data[1].timeframes.monthly.previous+`hrs`;
         /**monthly Study-Data**/
-        document.querySelector(".s-current").innerText = data[2].timeframes.monthly.current;
-        document.querySelector(".s-previous").innerText = data[2].timeframes.monthly.previous;
+        document.querySelector(".s-current").innerText = data[2].timeframes.monthly.current+`hrs`;
+        document.querySelector(".s-previous").innerText = data[2].timeframes.monthly.previous+`hrs`;
         /**monthly Exercise-Data**/
-        document.querySelector(".e-current").innerText = data[3].timeframes.monthly.current;
-        document.querySelector(".e-previous").innerText = data[3].timeframes.monthly.previous;
+        document.querySelector(".e-current").innerText = data[3].timeframes.monthly.current+`hrs`;
+        document.querySelector(".e-previous").innerText = data[3].timeframes.monthly.previous+`hrs`;
         /**monthly Social-Data**/
-        document.querySelector(".so-current").innerText = data[4].timeframes.monthly.current;
-        document.querySelector(".so-previous").innerText = data[4].timeframes.monthly.previous;
+        document.querySelector(".so-current").innerText = data[4].timeframes.monthly.current+`hrs`;
+        document.querySelector(".so-previous").innerText = data[4].timeframes.monthly.previous+`hrs`;
         /**monthly Self Care-Data**/
-        document.querySelector(".se-current").innerText = data[5].timeframes.monthly.current;
-        document.querySelector(".se-previous").innerText = data[5].timeframes.monthly.previous;
+        document.querySelector(".se-current").innerText = data[5].timeframes.monthly.current+`hrs`;
+        document.querySelector(".se-previous").innerText = data[5].timeframes.monthly.previous+`hrs`;
     }
 })
 
